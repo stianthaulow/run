9d84afc518ac1ffc11601a9cf246c831091ec717
Stian Thaulow <stian@thaulow.me>
2020-05-24 12:32:08 +0200

    Updates

---
+++ precache-manifest.13d30344288a57c56d5e8c8c4c68c3d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bda490d35cfe2adaf9d808004639ef9",
+    "url": "/run/index.html"
+  },
+  {
+    "revision": "b292369a515d8719b3ab",
+    "url": "/run/static/css/main.732abafc.chunk.css"
+  },
+  {
+    "revision": "8fb5556f3ff2134b1f84",
+    "url": "/run/static/js/2.01f07b30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/run/static/js/2.01f07b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b292369a515d8719b3ab",
+    "url": "/run/static/js/main.1025a20f.chunk.js"
+  },
+  {
+    "revision": "9c1b938ff535e3b9b105",
+    "url": "/run/static/js/runtime-main.fc197215.js"
+  }
+]);
