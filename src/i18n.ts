74d676997b0004ce67657317efc307ce1c2deb40
Stian Thaulow <stian@thaulow.me>
2024-09-23 18:25:31 +0200

    fix: auto import error

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import { l } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 import { initReactI18next } from "react-i18next";
 
 const resources = {
