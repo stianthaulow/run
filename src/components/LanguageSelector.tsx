45652ee76e27286d2403faa7b9c40da318de2346
Stian Thaulow <stian@thaulow.me>
2024-09-23 18:27:54 +0200

    feat: use flags in language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -9,26 +9,42 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useTranslation } from "react-i18next";
+import norwegianFlag from "@/assets/no.svg";
+import englishFlag from "@/assets/us.svg";
 
 export function LanguageSelector() {
   const { t, i18n } = useTranslation();
 
+  const flag = i18n.language === "no" ? norwegianFlag : englishFlag;
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">Open</Button>
+        <Button variant="outline" size="icon">
+          <img src={flag} alt={t("language.change")} className="size-6" />
+        </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
+      <DropdownMenuContent className="mr-2">
         <DropdownMenuLabel>{t("language.change")}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuRadioGroup
           value={i18n.language}
           onValueChange={i18n.changeLanguage}
         >
-          <DropdownMenuRadioItem value="no">
+          <DropdownMenuRadioItem value="no" className="flex gap-2">
+            <img
+              src={norwegianFlag}
+              alt={t("language.norwegianFlag")}
+              className="size-4"
+            />
             {t("language.norwegian")}
           </DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="en">
+          <DropdownMenuRadioItem value="en" className="felx gap-2">
+            <img
+              src={englishFlag}
+              alt={t("language.englishFlag")}
+              className="size-4"
+            />
             {t("language.english")}
           </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,15 +10,15 @@ export function Layout({ children }: LayoutProps) {
   const isTabletOrLarger = useMediaQuery("(min-width: 640px)");
 
   return (
-    <>
-      <LanguageSelector />
-      <main className="flex min-h-screen items-center justify-center p-4">
+    <div className="flex min-h-screen p-2 justify-between">
+      <main>
         {isTabletOrLarger ? (
           <Card className="w-full max-w-md p-6">{children}</Card>
         ) : (
           <div className="w-full">{children}</div>
         )}
       </main>
-    </>
+      <LanguageSelector />
+    </div>
   );
 }
