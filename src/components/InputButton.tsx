f9b42eddfe850569058b3b57777dd92796a1b8ab
Stian Thaulow <stian@thaulow.me>
2024-10-05 19:16:55 +0200

    fix: inputbutton text-alignment

--- src/components/InputButton.tsx
+++ src/components/InputButton.tsx
@@ -7,7 +7,7 @@ export function InputButton({ handleInputMode, value }: InputButtonProps) {
   return (
     <button
       type="button"
-      className="rounded pb-1 text-2xl focus:outline-dotted focus:outline-2 focus:outline-zinc-500 focus:outline-offset-2"
+      className="rounded text-left text-2xl focus:outline-dotted focus:outline-2 focus:outline-zinc-500 focus:outline-offset-2"
       onClick={handleInputMode}
       onKeyDown={(e) => {
         if (e.key === "Enter") {
